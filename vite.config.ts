399793681c49e370ab37d13f2e9025be758d3433
ogaru-sh <ogaru.sosh@gmail.com>
2025-02-18 23:04:41 +0900

    init app

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
+import adapter from "@hono/vite-dev-server/cloudflare";
 import { reactRouter } from "@react-router/dev/vite";
-import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
+import serverAdapter from "hono-react-router-adapter/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { getLoadContext } from "./load-context";
 
-export default defineConfig(({ isSsrBuild }) => ({
-  build: {
-    rollupOptions: isSsrBuild
-      ? {
-          input: "./workers/app.ts",
-        }
-      : undefined,
+export default defineConfig((_) => ({
+  ssr: {
+    resolve: {
+      externalConditions: ["workerd", "worker"],
+    },
   },
   plugins: [
-    cloudflareDevProxy({
-      getLoadContext({ context }) {
-        return { cloudflare: context.cloudflare };
-      },
+    serverAdapter({
+      adapter,
+      entry: "server/index.ts",
+      getLoadContext,
     }),
     tailwindcss(),
     reactRouter(),
